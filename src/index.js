b20c92e280bea9f5f4d20fe84fe50a7b9ec4064d
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2023-08-27 16:53:04 +0300

    update hw-10

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ const textLoader = document.querySelector('.loader');
 const textError = document.querySelector('.error');
 const catInfo = document.querySelector(".cat-info");
 
-textError.style.display = 'none'; 
+textError.style.display = 'none';
+
 window.addEventListener('DOMContentLoaded', () => {
-    
+    breedSelect.insertAdjacentHTML("afterbegin", `<option>Select_breed</option>`);
     breedSelect.style.display = 'none';
     setTimeout(() => {
         fetchBreeds()    
         .then(data => {
-        
+            
             breedSelect.style.display = 'block'
             textLoader.style.display = 'none'
             breedSelect.insertAdjacentHTML("beforeend", createMarkup(data))
@@ -39,35 +40,33 @@ function handlerChange() {
     catInfo.style.display = 'none';
     setTimeout(() => {
         fetchCatByBreed(selectedBreedId)
-            .then((data => { 
-                                
-                if (data[0] === undefined ) {
-                    console.log(data[0]);
-                    
-                    textError.style.display = 'block';
-                    textError.style.color = 'tomato';
-                    textLoader.style.display = 'none';
-                    
-                } else {
-                    textLoader.style.display = 'none';
-                    catInfo.style.display = 'block';
-                   console.dir(data[0].breeds[0]);
-                    const { url, width, height, breeds } = data[0]
-                    catInfo.innerHTML = `<div > <img class="cat" src="${url}" alt="${data[0].breeds[0].name}" width="${width * 0.3}" height="${height * 0.3}"/>
-                    <h2>${breeds[0].name}</h2>
-                    <p>${breeds[0].description}</p>
-                    <h3>Temperament:${breeds[0].temperament}</h3></div>`
-                }
-               
-            }))
-
+        .then((data => { 
+                            
+            if (data[0] === undefined ) {                
+                
+                textError.style.display = 'block';
+                textError.style.color = 'tomato';
+                textLoader.style.display = 'none';
+                
+            } else {
+                textLoader.style.display = 'none';
+                catInfo.style.display = 'block';
+                console.dir(data[0].breeds[0]);
+                const { url, width, height, breeds } = data[0]
+                catInfo.innerHTML = `<div > <img class="cat" src="${url}" alt="${data[0].breeds[0].name}" width="${width * 0.3}" height="${height * 0.3}"/>
+                <h2>${breeds[0].name}</h2>
+                <p>${breeds[0].description}</p>
+                <h3>Temperament:${breeds[0].temperament}</h3></div>`
+            }
+            
+        }))
     
-                .catch((error) => {
-                    console.log(error)
-                    textLoader.style.display = 'none'
-                    
-                })
-  
+        .catch((error) => {
+            console.log(error)
+            textLoader.style.display = 'none'
+            
+        })
+
         }, 600)
     
     
